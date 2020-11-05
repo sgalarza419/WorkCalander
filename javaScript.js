f9f98f7d37fa188f2b86681d9abad0fbca309623
S Galarza <sgalarza4190@gmail.com>
2020-11-05 18:16:32 -0500

    attempt to fix the user input

--- javaScript.js
+++ javaScript.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     //adding the colors for specfic time 
     function timeZone() {
         var eventTime = moment().format('h');
-        console.log(eventTime);
+        // console.log(eventTime);
         var currentTime = $('.row');
 
         currentTime.each(function () {
@@ -14,7 +14,7 @@ $(document).ready(function () {
             if (match < 9) {
                 match += 12;
             }
-            console.log(eventTime, match);
+            // console.log(eventTime, match);
             let time = $(this)[0];
             if (eventTime === match) {
                 $(time).addClass('present')
@@ -27,3 +27,72 @@ $(document).ready(function () {
     }
     timeZone();
 });
+
+dailyInput = $("textarea");
+// console.log(dailyInput);
+saveButton = $("button");
+// console.log(saveButton);
+
+
+timeRow = $(".row");
+// console.log(timeRow);
+
+saveButton.click(function () {
+
+    note = dailyInput.val();
+    console.log(note);
+//     noteTime = timeRow.attr("id");
+//     console.log(noteTime);
+
+//     if (noteTime && note) {
+//         localStorage.setItem(noteTime, note);
+//         location.reload();
+//     }
+});
+
+// savedNote = $("textarea");
+// for (i = 0; i < localStorage.length; i++) {
+//     value = localStorage.value(i);
+
+//     savedNote.innerHTML += '${value}<br />';
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var timeSlot = ["", "", "", "", "", "", "", ""];
+
+function loadData() {
+    //grab timeslot data from local storage ‘get item’
+    //loop thru timeSlot on screen
+    //for each row on the screen check timeslot array for non empty string
+    //set up eventTime, get the match < 9 ‘+=12’
+    //calculate timeSlot index = to match - 9
+    //if non empty string set textarea for row to string in the arrow pointed by index
+    //
+}
+//function save data
+function saveData() {
+    //event handled when you click save button
+    //event time
+    //convert to military time ‘+12’
+    //set index = military time - 9
+    //save incoming text in array
+    //save array in local storage
+}

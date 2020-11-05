2cf2a4a89acf5cc2d9d7b7dcdc77dda6bc1e49e3
S Galarza <sgalarza4190@gmail.com>
2020-11-04 19:25:47 -0500

    fixed color change of rows

--- javaScript.js
+++ javaScript.js
@@ -2,29 +2,28 @@ $(document).ready(function () {
     //Set the time
     var specificDay = moment().format('MMMM Do YYYY');
     $("#currentDay").text(specificDay);
-    var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
 
     //adding the colors for specfic time 
     function timeZone() {
-      var eventTime = moment().hour();
-      console.log(eventTime);
-      var currentTime = $('.row');
+        var eventTime = moment().format('h');
+        console.log(eventTime);
+        var currentTime = $('.row');
 
-      currentTime.each(function () {
-        var match = parseInt($(this).attr('id'))
-        if (match < 9) {
-          match += 12;
-        }
-        console.log(eventTime, match);
-        let time = $(this)[0];
-        if (eventTime === match) {
-          $(time).addClass('present')
-        } else if (eventTime > match) {
-          $(time).addClass('past')
-        } else if (eventTime < match) {
-          $(time).addClass('future')
-        }
-      })
+        currentTime.each(function () {
+            var match = parseInt($(this).attr('id'))
+            if (match < 9) {
+                match += 12;
+            }
+            console.log(eventTime, match);
+            let time = $(this)[0];
+            if (eventTime === match) {
+                $(time).addClass('present')
+            } else if (eventTime > match) {
+                $(time).addClass('past')
+            } else if (eventTime < match) {
+                $(time).addClass('future')
+            }
+        })
     }
     timeZone();
-  });
+});
